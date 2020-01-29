25c4bd417f3fd78e88041c3e5d60f6e6e05434c0
jaybubs <jkubofficial@gmail.com>
2020-01-30 00:06:17 +0800

    updoot for nginx and docker

--- includes/dbconxn.php
+++ includes/dbconxn.php
@@ -1,3 +1,3 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=jokes;charset=utf8','chicken','kokoko');
+    $pdo = new PDO('mysql:host=mariadb;dbname=jokes;charset=utf8','chicken','kokoko');
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
